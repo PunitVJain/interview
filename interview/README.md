92c39191aca62666c390c17c639571c6d64ff09e
Punit Vinod Jain <33111913+PunitVJain@users.noreply.github.com>
2021-01-17 10:36:27 +0530

    Delete interview directory

--- interview/README.md
+++
@@ -1,4 +0,0 @@
-# interview
-https://www.github.com/punit/code-20200108-punitjain
-17/01/2021 :-  Modified code without selenium.
-
--- interview/website/firstapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from firstapp.models import Uniinf, Program_Highlights
-# Register your models here.
-
-admin.site.register(Uniinf),
-admin.site.register(Program_Highlights),
